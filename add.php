bc520f98ce24a448add221133373308cea3a7204
Samuel Yi Ming Chen <sche0005@student.monash.edu>
2020-03-03 19:00:32 +1100

    Add new project

---
+++ add.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="add-student-header">
+            <h1>Add a Student</h1>
+        </div>
+        <div class="add-student-main">
+            <form class="add-student-form" action="store.html" method="post">
+
+            </form>
+        </div>
+    </div>
+  </body>
+</html>
