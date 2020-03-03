29bec2214f7ab2b251e271341984a79815714830
Samuel Yi Ming Chen <sche0005@student.monash.edu>
2020-03-03 19:21:46 +1100

    Add mysql script

---
+++ table.sql
@@ -0,0 +1,16 @@
+drop table if exists students cascade;
+
+CREATE TABLE students(
+  id                INTEGER NOT NULL auto_increment,
+  first_name        VARCHAR(255)NOT NULL,
+  last_name         VARCHAR(255),
+  dob               DATETIME,
+  school_year       INT,
+  cust_phone        VARCHAR(255),
+  mobile            VARCHAR(255),
+  first_contact_name  VARCHAR(255),
+  first_contact_phone VARCHAR(255),
+  second_contact_name  VARCHAR(255),
+  second_contact_phone VARCHAR(255),
+  PRIMARY KEY(id)
+);
